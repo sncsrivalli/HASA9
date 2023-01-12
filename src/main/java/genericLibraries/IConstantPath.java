b1b31a94174918ae6c42482d89ee5f9341f75557
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-01-12 06:16:59 +0530

    11/01/2023

---
+++ src/main/java/genericLibraries/IConstantPath.java
@@ -0,0 +1,14 @@
+package genericLibraries;
+
+/**
+ * This interface stores all the file paths used in the framework
+ * @author QPS-Basavanagudi
+ *
+ */
+public interface IConstantPath {
+	
+	String PROPERTY_FILE_PATH = "./src/test/resources/commonData.properties";
+	String EXCEL_FILE_PATH = "./src/test/resources/TestScriptData.xlsx";
+	String DATABASE_URL = "jdbc:mysql://localhost:3306/hasa9";
+
+}
