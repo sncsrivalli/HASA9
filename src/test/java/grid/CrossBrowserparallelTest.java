6d92f73ddfa5c072e3fff96994b75fe74b81db04
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-02-02 19:55:36 +0530

    2/02/2023

--- src/test/java/grid/CrossBrowserparallelTest.java
+++ src/test/java/grid/CrossBrowserparallelTest.java
@@ -7,22 +7,29 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class Practice1 {
+public class CrossBrowserparallelTest {
+	WebDriver driver;
+	@Parameters("Browser")
+	@BeforeMethod
+	public void methodConfiguration(String BROWSER) throws MalformedURLException {
+		URL url = new URL("http://192.168.1.123:4444/wd/hub");
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setPlatform(Platform.WINDOWS);
+		if(BROWSER.equals("chrome"))
+			cap.setBrowserName("chrome");
+		else if(BROWSER.equals("firefox"))
+			cap.setBrowserName("firefox");
+		
+		driver = new RemoteWebDriver(url, cap);
+		driver.manage().window().maximize();
+	}
 	
 	@Test
-	public void demo() throws MalformedURLException, InterruptedException {
-		
-		URL url = new URL("http://192.168.1.123:4444/wd/hub");
-		
-		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setBrowserName("chrome");
-		cap.setPlatform(Platform.WINDOWS);
-		
-		WebDriver driver = new RemoteWebDriver(url, cap);
-		
-		driver.manage().window().maximize();
+	public void test() throws InterruptedException {
 		driver.get("https://www.google.com/");
 		Thread.sleep(3000);
 		driver.quit();
